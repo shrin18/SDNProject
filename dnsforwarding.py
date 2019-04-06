45674877ddfaa3f1ac82feca91049831b37e170e
Shrinish <shdonde@gmail.com>
2019-04-06 11:56:26 +0200

    file updat

--- dnsforwarding.py
+++ dnsforwarding.py
@@ -1,91 +1,3 @@
-
-#! /usr/bin/env python3
-
-from scapy.all import DNS, DNSQR, DNSRR, IP, send, sniff, sr1, UDP
-
-IFACE = "lo0"   # Or your default interface
-DNS_SERVER_IP = "127.0.0.1"  # Your local IP
-
-BPF_FILTER = f"udp port 53 and ip dst {DNS_SERVER_IP}"
-
-
-def dns_responder(local_ip: str):
-
-    def forward_dns(orig_pkt: IP):
-        print(f"Forwarding: {orig_pkt[DNSQR].qname}")
-        response = sr1(
-            IP(dst='8.8.8.8')/
-                UDP(sport=orig_pkt[UDP].sport)/
-                DNS(rd=1, id=orig_pkt[DNS].id, qd=DNSQR(qname=orig_pkt[DNSQR].qname)),
-            verbose=0,
-        )
-        resp_pkt = IP(dst=orig_pkt[IP].src, src=DNS_SERVER_IP)/UDP(dport=orig_pkt[UDP].sport)/DNS()
-        resp_pkt[DNS] = response[DNS]
-        send(resp_pkt, verbose=0)
-        return f"Responding to {orig_pkt[IP].src}"
-
-    def get_response(pkt: IP):
-        if (
-            DNS in pkt and
-            pkt[DNS].opcode == 0 and
-            pkt[DNS].ancount == 0
-        ):
-            if "trailers.apple.com" in str(pkt["DNS Question Record"].qname):
-                spf_resp = IP(dst=pkt[IP].src)/UDP(dport=pkt[UDP].sport, sport=53)/DNS(id=pkt[DNS].id,ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname, rdata=local_ip)/DNSRR(rrname="trailers.apple.com",rdata=local_ip))
-                send(spf_resp, verbose=0, iface=IFACE)
-                return f"Spoofed DNS Response Sent: {pkt[IP].src}"
-
-            else:
-                # make DNS query, capturing the answer and send the answer
-                return forward_dns(pkt)
-
-    return get_response
-
-sniff(filter=BPF_FILTER, prn=dns_responder(DNS_SERVER_IP), iface=IFACE)
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-	
 #! /usr/bin/env python3
  
 from scapy.all import DNS, DNSQR, DNSRR, IP, send, sniff, sr1, UDP
@@ -93,7 +5,7 @@ from scapy.all import DNS, DNSQR, DNSRR, IP, send, sniff, sr1, UDP
 IFACE = "lo0"   # Or your default interface
 DNS_SERVER_IP = "127.0.0.1"  # Your local IP
  
-BPF_FILTER = f"udp port 53 and ip dst {DNS_SERVER_IP}"
+BPF_FILTER = "udp port 53 and ip dst {DNS_SERVER_IP}"
  
  
 def dns_responder(local_ip: str):
@@ -101,8 +13,8 @@ def dns_responder(local_ip: str):
     def forward_dns(orig_pkt: IP):
         print(f"Forwarding: {orig_pkt[DNSQR].qname}")
         response = sr1(
-            IP(dst='8.8.8.8')/
-                UDP(sport=orig_pkt[UDP].sport)/
+            IP(dst='8.8.8.8')
+                UDP(sport=orig_pkt[UDP].sport)
                 DNS(rd=1, id=orig_pkt[DNS].id, qd=DNSQR(qname=orig_pkt[DNSQR].qname)),
             verbose=0,
         )
