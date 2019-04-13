88c71a4b9e9d243fed05edac822174d1539eb617
Shrinish Donde <shdonde@gmail.com>
2019-04-13 12:20:25 +0200

    Update dnsrequest.py

--- dnsrequest.py
+++ dnsrequest.py
@@ -2,9 +2,7 @@
  
 from scapy.all import DNS, DNSQR, IP, sr1, UDP
  
-dns_req = IP(dst='8.8.8.8')
-UDP(dport=53)
-DNS(rd=1, qd=DNSQR(qname='www.thepacketgeek.com'))
+dns_req = IP(dst='8.8.8.8')/UDP(dport=53)/DNS(rd=1, qd=DNSQR(qname='www.thepacketgeek.com'))
 answer = sr1(dns_req, verbose=0)
  
 print(answer[DNS].summary())
